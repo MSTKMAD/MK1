27b9512763a238a19ff0b6025ec7ac9df438e09b
Jmusotoku <javier@musotoku.com>
2023-08-10 16:56:05 +0200

    Archivos necesarios para la compilacion.
    Compile.bat
    Upload2RP
    Librerias

---
+++ 03.Firmware/MK1-RP2040/740/EDCPSU-TATTOO/Upload2RP.bat
@@ -0,0 +1,14 @@
+
+@echo off
+:: Board a usar en la compilacion
+set board=rp2040:rp2040:generic
+set port=COM3
+set sketch=.\EDCPSU-TATTOO.ino
+set libraries="C:/Users/javie/Team Dropbox/JRODRIGUEZ/Repositorios/11.MK1/03.Firmware/Libraries"
+
+:: Comando para compilar , exportar y uploadear los archivos binarios en la carpeta residente de este .bat
+
+arduino-cli compile -e -t --fqbn %board% -v --libraries %libraries%
+
+arduino-cli upload %sketch% --fqbn %board% --port %port%
+
